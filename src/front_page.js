4b7d2f00c85c0340817e05c78bbcc9622e431f19
dao vo <gary.vo32@gmail.com>
2022-04-08 18:45:29 -0700

    expanded contain class' height and width

--- src/front_page.js
+++ src/front_page.js
@@ -1,22 +1,22 @@
 function home() {
-
-  if (!document.querySelector('nav')) {
-    let navbar = nav()
+  if (!document.querySelector("nav")) {
+    let navbar = nav();
     content.appendChild(navbar);
   }
 
-  let restaurantName = document.createElement('h1');
-  let restaurantDesc = document.createElement('p');
-  let restaurantPic = document.createElement('img');
-  let content = document.getElementById('content');
-  let contain = document.createElement('section');
+  let restaurantName = document.createElement("h1");
+  let restaurantDesc = document.createElement("p");
+  let restaurantPic = document.createElement("img");
+  let content = document.getElementById("content");
+  let contain = document.createElement("section");
 
-  contain.classList.add('contain')
+  contain.classList.add("contain");
+  contain.classList.add("content");
 
-  restaurantName.textContent = 'Daygo Dining'
-  restaurantDesc.textContent = 'Yummy food place, number 1!'
-  restaurantPic.src = '../loes-klinker-gPzixog6XLg-unsplash.jpg'
-  restaurantPic.alt = 'viet food'
+  restaurantName.textContent = "Daygo Dining";
+  restaurantDesc.textContent = "Yummy food place, number 1!";
+  restaurantPic.src = "../images/pho.jpg";
+  restaurantPic.alt = "viet food";
 
   content.appendChild(contain);
   contain.appendChild(restaurantName);
