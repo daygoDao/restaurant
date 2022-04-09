29f9f2ce225c6c8bceae03b9d86487367a606c97
dao vo <gary.vo32@gmail.com>
2022-04-09 14:11:45 -0700

    arrows in food gallery can loop through but now need to remove old so only 1 picture is displayed and not appened to

--- src/menu.js
+++ src/menu.js
@@ -1,34 +1,57 @@
 function menu() {
   const pictures = ["pho", "clams", "pork_assorted", "sexybowl"];
+  let index = 0;
 
   let restaurantName = document.createElement("h1");
   let content = document.getElementById("content");
   let contain = document.createElement("section");
   let foodGallery = document.createElement("section");
   contain.classList.add("contain");
-  foodGallery.classList.add('food_gallery')
+  foodGallery.classList.add("food_gallery");
 
   content.appendChild(contain);
   restaurantName.textContent = "Picture Gallery";
   contain.appendChild(restaurantName);
 
-  for (let food of pictures) {
+  //display food 1 at a time on an interval or manual by arrows
+  function showFood(picturesIndex) {
     let restaurantPic = document.createElement("img");
 
-    restaurantPic.src = `../images/${food}.jpg`;
+    restaurantPic.src = `../images/${pictures[picturesIndex]}.jpg`;
     restaurantPic.alt = "viet food";
     foodGallery.appendChild(restaurantPic);
+  
+    contain.append(foodGallery);
   }
-  contain.append(foodGallery)
+
+  showFood(0);
 
   //buttons to traverse through food
   //loops back to 0th index after the end of picture array is reached
-  const rightButton = document.createElement('button');
-  const leftButton = document.createElement('button');
-  leftButton.textContent = '<'
-  rightButton.textContent = '>'
-  contain.append(leftButton)
-  contain.append(rightButton)
+  const rightButton = document.createElement("button");
+  const leftButton = document.createElement("button");
+  leftButton.textContent = "<";
+  rightButton.textContent = ">";
+  leftButton.addEventListener('click',leftPicture)
+  rightButton.addEventListener('click',rightPicture)
+
+  function leftPicture() {
+    if(index == 0) {
+      index = pictures.length;
+    }
+    index--;
+    showFood(index);
+  }
+  function rightPicture() {
+    if(index == pictures.length - 1) {
+      index = 0;
+    }
+    index++;
+    showFood(index);
+  }
+
+  contain.append(leftButton);
+  contain.append(rightButton);
 }
 
 export default menu;
