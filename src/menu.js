3c08e9ba58db1ae55779728e91d059c64bc7b360
dao vo <gary.vo32@gmail.com>
2022-04-08 18:08:46 -0700

    made an array of food pics menu section

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,25 @@
 function menu() {
-  let restaurantName = document.createElement('h1');
-  let restaurantPic = document.createElement('img');
-  let content = document.getElementById('content');
-  let contain = document.createElement('section')
-  contain.classList.add('contain')
+  const pictures = ["pho", "clams", "pork_assorted", "sexybowl"];
+
+  let restaurantName = document.createElement("h1");
+  let content = document.getElementById("content");
+  let contain = document.createElement("section");
+  let foodGallery = document.createElement("section");
+  contain.classList.add("contain");
+  foodGallery.classList.add('food_gallery')
 
   content.appendChild(contain);
-
-  restaurantName.textContent = 'Picture Gallery'
+  restaurantName.textContent = "Picture Gallery";
   contain.appendChild(restaurantName);
 
-  restaurantPic.src = '../loes-klinker-gPzixog6XLg-unsplash.jpg'
-  restaurantPic.alt = 'viet food'
-  contain.appendChild(restaurantPic);
+  for (let food of pictures) {
+    let restaurantPic = document.createElement("img");
 
-  restaurantPic.src = '../loes-klinker-gPzixog6XLg-unsplash.jpg'
-  restaurantPic.alt = 'viet food'
-  contain.appendChild(restaurantPic);
+    restaurantPic.src = `../images/${food}.jpg`;
+    restaurantPic.alt = "viet food";
+    foodGallery.appendChild(restaurantPic);
+  }
+  contain.append(foodGallery)
 }
 
 export default menu;
