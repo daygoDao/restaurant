022822e4962665424bfeb021e19e241114b592ec
dao vo <gary.vo32@gmail.com>
2022-04-11 13:50:07 -0700

    able to display single food obj to the world of the menu dom

--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,36 @@
 function menu() {
   // class to create food item
   const food = (name, desc) => {
-    const itemDOM = document.createElement('section')
-    itemDOM.textContent = name;
+    // return item DOM section to later append to menu section
+    const itemDOM = document.createElement("section");
+    itemDOM.classList.add("food");
+    // picture of food
+    /*  const picture = document.createElement('img')
+    picture.alt = `${name}` */
+    // h3: name of food
+    const displayName = document.createElement("h3");
+    displayName.textContent = `${name}`;
+    // p: desc of food
 
-    return {name,desc}
-  }
+    itemDOM.appendChild(displayName)
+
+    return itemDOM;
+  };
 
   // class to create menu containing food items
   const createMenu = () => {
-    const menuDOM = document.createElement('section');
-    menuDOM.classList.add('menu')
-    
-    // menuDOM.appendChild(food('pho','its pho dude'))
+    const menuDOM = document.createElement("section");
+    menuDOM.classList.add("menu");
+
+    menuDOM.appendChild(food("pho", "its pho dude"));
 
     return menuDOM;
-  }
+  };
 
   // function to create and load menu to page
-  const displayMenu = () => {
-
-  }
-  console.log(createMenu())
-  // document.querySelector('#content').appendChild(createMenu())
-
+  // const displayMenu = () => {};
+  console.log(createMenu());
+  document.querySelector('#content').appendChild(food('to','toto'))
 }
 
 export default menu;
